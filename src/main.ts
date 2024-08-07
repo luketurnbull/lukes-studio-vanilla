49976e68650d28f90afd59be7d561bba12bdcbb8
Luke David Turnbull <lukedturnbull@gmail.com>
2024-08-07 21:07:56 +1000

    Create initial grid layout

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,21 @@
 import "./style.css";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    <h1>Luke's Studio - Vanilla</h1>
-    <div>
+  <div class="container">
+    <header class="header">
+      <h1>Luke's Studio - Vanilla</h1>
+    </header>
+
+    <nav class="sidebar">
       <ul>
-        <li>Web Components</li>
-        <li>MDN Docks</li>
-        <li>JavaScript</li>
-        <li>Typescript</li>
-        <li>Data structures and algorithms</li>
-        <li>Web APIS</li>
-        <li>ThreeJS</li>
+        <li><a href="#">Three</a></li>
+        <li><a href="#">Data Structures & Algorithms</a></li>
+        <li><a href="#">Web APIs</a></li>
       </ul>
-    </div>
+    </nav>
+
+    <main class="main">
+      <h2>Vanilla</h2>
+    </main>
   </div>
 `;

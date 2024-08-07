8b5eaba2a85143b5b2a1923d86248f740c5b2463
Luke David Turnbull <lukedturnbull@gmail.com>
2024-08-07 20:39:13 +1000

    Delete initial vite counter code

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,18 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+    <h1>Luke's Studio - Vanilla</h1>
+    <div>
+      <ul>
+        <li>Web Components</li>
+        <li>MDN Docks</li>
+        <li>JavaScript</li>
+        <li>Typescript</li>
+        <li>Data structures and algorithms</li>
+        <li>Web APIS</li>
+        <li>ThreeJS</li>
+      </ul>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
   </div>
-`
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+`;
